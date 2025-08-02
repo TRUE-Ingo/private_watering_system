eb3748e2ea67b2ccf341154c8128a2570f43aefd
Ingo Boegemann <85360585+TRUE-Ingo@users.noreply.github.com>
2025-08-02 09:24:16 +0200

    some fixes in chart and threshold setting

--- backend/server.js
+++ backend/server.js
@@ -40,7 +40,7 @@ app.use(helmet({
   contentSecurityPolicy: {
     directives: {
       defaultSrc: ["'self'"],
-      scriptSrc: ["'self'", "'unsafe-inline'"],
+      scriptSrc: ["'self'", "'unsafe-inline'", "https://cdn.jsdelivr.net"],
       styleSrc: ["'self'", "'unsafe-inline'"],
       imgSrc: ["'self'", "data:", "https:"],
       connectSrc: ["'self'"],
